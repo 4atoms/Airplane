e69c3aeec98f04a8835a937ad9620ec56a806cd7
umeshbajaj025 <umesh.bajaj@4atoms.se>
2021-06-22 15:20:28 +0530

    init

---
+++ Components/hello.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Hello=()=>{
+    return <div>
+    Archit
+    </div>
+}
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
+import Hello from '../Components/hello';
 ReactDOM.render(
     <div>Hello World</div>,
     document.getElementById("app-root")
