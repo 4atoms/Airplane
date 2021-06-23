9d6495f5b8ff9ba269273217954b1cfd64dcd1da
umeshbajaj025 <umesh.bajaj@4atoms.se>
2021-06-23 13:39:02 +0530

    init

--- src/Components/hello.js
+++
@@ -1,8 +0,0 @@
-export default function Hello() {
-    return (
-      <div className="hello">
-        <h1>Hello World</h1>
-        <h2>Edit to see some magic happen!</h2>
-      </div>
-    );
-  }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
-import Hello from './Components/hello';
-const rootElement=document.getElementById("app-root");
 ReactDOM.render(
-    <div><Hello/></div>,rootElement
+    <div>Hello</div>,document.getElementById("app-root")
     
 );
