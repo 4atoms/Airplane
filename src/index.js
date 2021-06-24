25a41a117961435c111a4b3016caf0ea53964056
umeshbajaj025 <umesh.bajaj@4atoms.se>
2021-06-24 10:57:30 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
+import Navbar from './Navbar';
+import Travelblog from "./Travelblog";
+
 ReactDOM.render(
-    <div>Hello</div>,document.getElementById("app-root")
+    
+    <React.StrictMode>
+        {/* <div>Hello Archit</div> */}
+        <Navbar isColorRed={false} />
+        <Travelblog/>
+    </React.StrictMode>,
+    document.getElementById("app-root")
     
 );
