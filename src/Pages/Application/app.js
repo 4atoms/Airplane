79045d340d9d612dd85e43e05e414e70932a4639
umeshbajaj025 <umesh.bajaj@4atoms.se>
2021-06-25 10:34:31 +0530

    25th jun commit

--- src/Pages/Application/app.js
+++ src/Pages/Application/app.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import  Hotels  from  '../Hotels/hotels'
 import  Homepage  from '../Homepage/homepage';
@@ -11,22 +10,21 @@ import Flights from '../Flights/flights';
         
       <Router>
       <Switch>
-        <Route exact path="/" component={Homepage} />
+        {/* <Route exact path="/" component={Homepage} /> */}
         <Route  path="/Flights" component={Flights} />
-        
         <Route  path="/Hotels" component={Hotels} />
         
       </Switch>
     </Router>
-    <Router>
-        <div>
+    {/* <Router>
+      <div>
         <ul>
             <Link to="/Flights" >Flights  </Link>
             <Link to="/Hotels" >Hotels  </Link>
         </ul>
       </div>
-      </Router>
-      <div>Hi welcome to App</div>
+    </Router> */}
+      <div><Homepage/></div>
     </>
   );
 };
