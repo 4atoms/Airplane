e7aeee3b02fb607e4ce83b3ef4c515a9a1ea79eb
VaishnaviVinay <84966861+VaishnaviVinay@users.noreply.github.com>
2021-06-27 23:02:14 +0530

    Hotel page and style changes

--- src/Pages/Hotels/hotels.js
+++ src/Pages/Hotels/hotels.js
@@ -1,11 +1,85 @@
-import { HHeader } from "./hotels.style";
+import React, {useState, useEffect} from "react";
+import { InputContainer, Container,InputComponent,SelectComponent,OptionComponent,OuterContainer,ButtonComponent,InputElement } from "./hotels.style";
 import Navbar from '../Homepage/Navbar/Navbar';
+import Footer from "../Homepage/Footer/footer";
 const Hotels=()=>
 {
+
+    var puneAPI = "https://run.mocky.io/v3/dcb0a868-1e85-407e-9055-8fcc22bda502";
+    var bangaloreAPI = "https://run.mocky.io/v3/f0904fd1-78a1-4a5f-943d-e5775469660d";
+
+    const [value, setValue] = useState("Pune");
+    const [apiresult, setApiResult] = useState([]);
+
+    useEffect(() => {
+        console.log("Value changed for city : "+value);
+    }, [apiresult]);
+
+    const handleChange = (e) =>{        
+        console.log("city selected " +e.value)
+        setValue(e.value)
+
+    }
+
+    var hotelList;
+    const getHotels = async(city) => {
+        console.log("city name "+city.value)
+        var api;
+        switch(city.value){
+            case "Pune":
+                api = puneAPI;
+                break;
+            case "Bangalore":
+                api = bangaloreAPI;
+                break;
+        }
+        const response = await fetch(api)
+        const jsonArray = await response.json()
+                
+        hotelList = jsonArray.hotels;
+        console.log("api response",{hotelList});
+        const modifiedHotelArray = hotelList.map(hotel =>{
+            return{"name":hotel.name,"rating":hotel.rating}
+        });
+        setApiResult(modifiedHotelArray)    
+        console.log("changed value",{apiresult})
+        
+    }
+    
     return(
         <>
             <Navbar/>
-            <HHeader>Hotels</HHeader>
+            <OuterContainer>
+                <Container>
+                    Book Domestic hotels and homestays now!
+                </Container>
+                <InputContainer> 
+                    <InputElement>
+                        Select a city
+                        <SelectComponent id = "city" onChange = {(e) => handleChange({value : e.target.value})}> 
+                            <OptionComponent value = "Pune">Pune</OptionComponent>
+                            <OptionComponent value = "Mumbai">Mumbai</OptionComponent>
+                            <OptionComponent value = "Bangalore" selected = "selected">Bangalore</OptionComponent>
+                        </SelectComponent>
+                    </InputElement>
+                    <InputElement>
+                        Check in date
+                        <InputComponent type="date"/>
+                    </InputElement>
+                    <InputElement>
+                        Check out date
+                        <InputComponent type="date"/>
+                    </InputElement>
+                    <InputElement>
+                        Number of guests
+                        <InputComponent type="number"/>
+                    </InputElement>
+                </InputContainer>
+                <ButtonComponent>
+                    <InputComponent type="button" value = "Search!" onClick = {() => getHotels({value})}/>
+                </ButtonComponent>
+            </OuterContainer>
+            <Footer/>
         </>        
     );
 };
--- src/Pages/Hotels/hotels.style.js
+++ src/Pages/Hotels/hotels.style.js
@@ -1,8 +1,77 @@
 import styled from "styled-components";
+import img from "./vacation.jpg";
 
-export const HHeader=styled.div`
+export const InputContainer=styled.div`
+position: fixed;
+  top: 30% ;
+  left: 50%;
+  transform: translate(-50%, -50%);
 display:flex;
-justify-content:center;
-background-color:rgba(25,25,0,0.4);
-font-size:50px;
+justify-content:space-around;
+background-color:rgb(230, 230, 250);
+padding : 30px;
+align-items: center;
+border : solid;
+border-radius: 2em;
+`;
+
+
+export const Container=styled.div`
+position: fixed;
+  top: 20% ;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index : 2;
+display:flex;
+background-color:rgb(230, 230, 250);
+font-size:10px;
+padding : 15px;
+border : solid;
+border-radius: 1em;
+align-items: center;
+`;
+
+export const ButtonComponent=styled.div`
+position: fixed;
+  top: 38% ;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index : 2;
+display:flex;
+background-color:rgb(230, 230, 250);
+font-size:10px;
+padding : 10px;
+border : solid;
+border-radius: 1em;
+align-items: center;
+`;
+
+export const InputElement=styled.div`
+width : 20%;
+font-size:10px;
+box-sizing : border-box;
+`;
+
+export const InputComponent=styled.input`
+
+font-size:10px;
+box-sizing : border-box;
+width:100%;
+`;
+
+export const OptionComponent=styled.option`
+
+font-size:10px;
+`;
+
+export const SelectComponent=styled.select`
+
+font-size:10px;
+`;
+
+export const OuterContainer = styled.div`
+background-image: url(${img});
+width : 100%;
+height : 100vh;
+background-size: contain;
 `;
