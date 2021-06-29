f9e4c5f5759011c1804459a29ba8f7d9e392dd75
Archit <archit@4atoms.se>
2021-06-29 21:06:51 +0530

    29th June Commit Final

--- src/Pages/Homepage/TravelBlog/Accordian.js
+++ src/Pages/Homepage/TravelBlog/Accordian.js
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import  {AiOutlineMinus, AiOutlinePlus} from 'react-icons/fa';
 
-const Accordian = ({ title, info }) => 
-{
+const Accordian = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
-  
-return (
- 
-   <div className='singleaccord'>
- 
-     <header>
-        
-		<h2 >{title}</h2>
 
-     	 <button className='btn' onClick={() => setShowInfo(!showInfo)}>
-       		{/* {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />} */}
-    	 </button>
-    
- 	</header>
-    	
-		{showInfo && <p style={{fontSize: '20px',color: '#7c795d',fontFamily: 'Arial',backgroundColor:'#fed8b1'}}>{info}</p>}
-		
-   </div>
-  
-);
+  return (
+    <div className="singleaccord">
+      <header>
+        <h2>{title}</h2>
 
+        <button className="btn" onClick={() => setShowInfo(!showInfo)}>
+          {/* {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />} */}
+        </button>
+      </header>
+
+      {showInfo && (
+        <p
+          style={{
+            fontSize: "20px",
+            color: "#7c795d",
+            fontFamily: "Arial",
+            backgroundColor: "#fed8b1",
+          }}
+        >
+          {info}
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default Accordian;
