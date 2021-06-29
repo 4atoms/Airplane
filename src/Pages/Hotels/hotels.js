d4ea238c049e061345671636612b0ad1ce0fc857
VaishnaviVinay <84966861+VaishnaviVinay@users.noreply.github.com>
2021-06-30 03:17:06 +0530

    Components changes

--- src/Pages/Hotels/hotels.js
+++ src/Pages/Hotels/hotels.js
@@ -11,8 +11,19 @@ import {
   HotelDetails,
   InputElement,
 } from "./hotels.style";
+<<<<<<< Updated upstream
 import Navbar from "../Homepage/Navbar/Navbar";
 import Footer from "../Homepage/Footer/footer";
+=======
+import XSlider from "../../Components/InputComponent/XSlider"
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/footer";
+
+var puneAPI = "https://run.mocky.io/v3/f13753b4-bde1-422d-aa5e-c34768e28d95";
+var bangaloreAPI =
+  "https://run.mocky.io/v3/56973ad0-0797-4c61-8fc8-c7a5e57ad79c";
+var goaAPI = "https://run.mocky.io/v3/24c6c3ad-9d4a-4a14-8e51-05f2a9e07fcf";
+>>>>>>> Stashed changes
 const Hotels = () => {
   var puneAPI = "https://run.mocky.io/v3/f13753b4-bde1-422d-aa5e-c34768e28d95";
   var bangaloreAPI = "https://run.mocky.io/v3/56973ad0-0797-4c61-8fc8-c7a5e57ad79c";
@@ -20,6 +31,7 @@ const Hotels = () => {
 
   const [value, setValue] = useState("Bangalore");
   const [apiresult, setApiResult] = useState([]);
+  const [rating, setRating] = useState("3");
 
   useEffect(() => {
     console.log("Value changed for city : " + apiresult);
@@ -29,6 +41,10 @@ const Hotels = () => {
     console.log("city selected " + e.value);
     setValue(e.value);
   };
+
+  const updateRating = (rating) => {
+      setRating(rating);
+  }
 
   var hotelList;
   const getHotels = async (city) => {
@@ -47,6 +63,7 @@ const Hotels = () => {
     }
     const response = await fetch(api);
     const jsonArray = await response.json();
+    
 
     hotelList = jsonArray.hotels;
     console.log("api response", { hotelList });
@@ -94,6 +111,9 @@ const Hotels = () => {
             Number of guests
             <InputComponent type="number" />
           </InputElement>
+          <InputElement>
+            <XSlider onRatingChange = {updateRating} id="customSelector" name = "rating-input" label = "Min rating : " min = "0" max = "5" type = "range" step = "0.5"/> 
+            </InputElement>
         </InputContainer>
         <ButtonComponent>
           <InputComponent
@@ -103,7 +123,7 @@ const Hotels = () => {
           />
         </ButtonComponent>
         <HotelContainer>
-          {apiresult.map((i) => {
+          {apiresult.filter(hotel => hotel.rating > rating).map((i) => {
             return (
               <HotelDetails ratings={i.rating}>
                 <div>
