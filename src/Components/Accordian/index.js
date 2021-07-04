42faa9f78d806d98c3c601141f996369fb581622
Archit <archit@4atoms.se>
2021-07-04 21:32:25 +0530

    Data coming from main(Accordian)

--- src/Components/Accordian/index.js
+++ src/Components/Accordian/index.js
@@ -1,30 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { Wrapper, Item, Title } from "./Accordian.style";
 
-const accordiandata = [
-  {
-    title: "title 1",
-    info: "#FlyLikeAStar with MakeMyTrip’s New Charter Flight Serv...",
-    id: 1,
-  },
-  {
-    title: "title 2",
-    info: "6 things to do on business trip",
-    id: 2,
-  },
-  {
-    title: "title 3",
-    info: "Your One-Stop Resource For Country-Wise Travel Guidelin...",
-    id: 3,
-  },
-];
-
-const Accordian = () => {
+const Accordian = ({ data1 }) => {
   const [selected, setSelected] = useState(null);
-  const [data, setData] = useState(accordiandata);
-
-  useEffect(() => {}, [data]);
+  const [data, setData] = useState(data1);
 
   const addItem = () => {
     console.log(data);
@@ -45,19 +25,17 @@ const Accordian = () => {
     <>
       <button onClick={addItem}>Add new Item</button>
       <Wrapper>
-        <div>
-          {data.map((accord, i) => (
-            // eslint-disable-next-line react/jsx-key
-            <Item key={i}>
-              <Title onClick={() => toggle(i)}>
-                <h2>{accord.title}</h2>
-              </Title>
-              <div className={selected === i ? "Infoshow" : "Info"}>
-                {accord.info}
-              </div>
-            </Item>
-          ))}
-        </div>
+        {data.map((accord, i) => (
+          // eslint-disable-next-line react/jsx-key
+          <Item key={i}>
+            <Title onClick={() => toggle(i)}>
+              <h2>{accord.title}</h2>
+            </Title>
+            <div className={selected === i ? "Infoshow" : "Info"}>
+              {accord.info}
+            </div>
+          </Item>
+        ))}
       </Wrapper>
     </>
   );
